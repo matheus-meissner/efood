98ea0e900761a425e9a69cfdb622a2bdad792dbc
Matheus Meissner <matheus.iembo@gmail.com>
2024-08-16 15:47:59 -0300

    Corrige diretório da pasta Pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Profile from './Pages/Profile'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 const Rotas = () => (
   <Routes>
